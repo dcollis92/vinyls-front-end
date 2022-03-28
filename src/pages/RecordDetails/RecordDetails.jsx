e0598745db2c3eb618ca87c594ff22330d52f823
Tim Crisp <timothycrisp@gmail.com>
2022-03-28 11:57:46 -0500

    record detail and component

--- src/pages/RecordDetails/RecordDetails.jsx
+++ src/pages/RecordDetails/RecordDetails.jsx
@@ -1,2 +1,19 @@
-// difference between this and component/record/record
+import { useLocation } from 'react-router-dom'
 
+const RecordDetails = () => {
+  const location = useLocation()
+  const record = location.state.record
+
+  return (
+    <>
+    <div className="record">
+      <h3>
+        {record.title}
+        {record.artists}
+      </h3>
+    </div>
+    </>
+  )
+} 
+
+export default RecordDetails;
