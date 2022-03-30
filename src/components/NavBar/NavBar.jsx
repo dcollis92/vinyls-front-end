48e0e0df119b0bca48d16e0294d2b15624020dd0
dcollis92 <davidphilipcollis@gmail.com>
2022-03-30 10:17:37 -0500

    css refactoring unfinished

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import * as recordService from '../../services/recordService'
-import { Nav, Navbar, Button, Form, FormControl, Container }  from 'react-bootstrap'
+import { Nav, Navbar, Button, Form, FormControl, Container }  from 'react-bootstrap';
 import './NavBar.scss';
 
 
