6f534918b216bc170625795fca1f2580094317f1
Tim Crisp <timothycrisp@gmail.com>
2022-03-30 21:57:41 -0500

    dio sucks

--- src/pages/UserCollection/UserCollection.jsx
+++ src/pages/UserCollection/UserCollection.jsx
@@ -1,20 +1,32 @@
 import './UserCollection.scss';
+import { Button } from 'react-bootstrap';
 import { useState, useEffect } from 'react'
+
 import Record from '../../components/Record/Record'
 
 const UserCollection = ({records, handleRemoveRecord, handleAddRecord, profile}) => {
+  
+  console.log(profile)
+  console.log(profile.records)
 
-console.log(profile.records)
+  
+
 
   return (
     <>
       <h2>Record Collection</h2>
+      <div>
   
         <div className=''>
-          {profile.records.map(record =>
-            <Record handleRemoveRecord={handleRemoveRecord} profile={profile} handleAddRecord={handleAddRecord} key={record.id} id={record._id} record={record}/>  
+          {profile?.records?.map(record =>
+          <>
+          <Record handleRemoveRecord={handleRemoveRecord} profile={profile} handleAddRecord={handleAddRecord} key={record.id} id={record._id} record={record}/>  
+          <Button onClick={() => handleRemoveRecord(profile._id, record._id)} profile={profile} variant="outline-success">Remove</Button>          
+          </>
           )}
         </div>
+        
+      </div>  
     </>
   );
 }
