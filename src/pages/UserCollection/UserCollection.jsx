0a87e60cd961a68ee90ae4f9a938145fb41bccaa
provideforme <evan.a.bernard@gmail.com>
2022-03-31 12:21:01 -0400

    refactor usercollection

--- src/pages/UserCollection/UserCollection.jsx
+++ src/pages/UserCollection/UserCollection.jsx
@@ -2,38 +2,41 @@ import './UserCollection.scss';
 import { Button } from 'react-bootstrap';
 import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import * as profileService from '../../services/profileService'
 import Profile from '../../components/Profile/Profile'
+import Record from '../../components/Record/Record';
 
-const UserCollection = () => {
-  
+const UserCollection = ({handleRemoveRecord}) => {
+  const [currProfile, setCurrProfile] = useState(null)
+
   const location = useLocation()
   const profile = location.state.profile
   
+  useEffect(()=> {
+    profileService.getProfile(profile._id)
+    .then(profile => setCurrProfile(profile))
+  }, [])
+
   return (
+    currProfile?
     <>
       <h2>Record Collection</h2>
       <div>
-        <Profile key={profile.name} />
-        <h2>{profile.name}</h2>
-      </div>
-
-
-
-      {/* <div>
-  
-        <div className=''>
-          {profile?.records?.map(record =>
+        <Profile key={currProfile.name} />
+        <h2>{currProfile.name}</h2>
+        <h2>{currProfile?.records?.map(record =>
           <>
-          <Record handleRemoveRecord={handleRemoveRecord} handleAddRecord=
-          {handleAddRecord} profile={profile} key={record.id} id={record._id} record={record}/>
-            
+          <Record handleRemoveRecord={handleRemoveRecord} profile={currProfile} key={record.id} id={record._id} record={record}/>
+          
           <Button onClick={() => handleRemoveRecord(profile._id, record._id)} profile={profile} variant="outline-success">Remove</Button>          
           </>
-          )}
           
-        </div>
-        
-      </div>   */}
+          )}</h2>
+      </div>
+    </>
+    :
+    <>
+    <div>loading...</div>
     </>
   ); 
 }
